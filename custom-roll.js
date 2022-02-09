0617e434473688327add8b69e4119853f17fa230
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-02-09 10:07:39 -0700

    created a catch for entering 0 or 1

--- custom-roll.js
+++ custom-roll.js
@@ -2,6 +2,11 @@ const prompt = require('prompt-sync')();
 
 let numSides = Number(prompt("How many sides would you like your dice to be? "));
 
+if (numSides === 0 || numSides === 1){
+    console.log("You can't really have a Zero-Sided or a One-Sided Dice in this realm. Run the program again and choose a number greater than 1.")
+    return;
+}
+
 let randNum = Math.random();
 
 let dice = Math.ceil(randNum * numSides);
